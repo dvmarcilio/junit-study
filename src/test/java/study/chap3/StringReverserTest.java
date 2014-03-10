5bc9029f7029febc71537b74e60d5cd21c918498
Diego Marcílio <dvmarcilio@gmail.com>
2014-03-10 17:30:55 -0300

    Parameterizing tests with JUnitParams

--- src/test/java/study/chap3/StringReverserTest.java
+++ src/test/java/study/chap3/StringReverserTest.java
@@ -1,9 +1,14 @@
 package study.chap3;
 
+import static junitparams.JUnitParamsRunner.$;
 import static org.junit.Assert.assertEquals;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(JUnitParamsRunner.class)
 public class StringReverserTest {
 
 	@Test
@@ -11,23 +16,19 @@ public class StringReverserTest {
 		String reverted = StringReverser.reverse(new String());
 		assertEquals(reverted, new String());
 	}
-	
+
 	@Test(expected = NullPointerException.class)
 	public void throwsNullExceptionOnNullArgument() {
 		StringReverser.reverse(null);
 	}
-	
-	@Test
-	public void revertsString() {
-		String s = "abcd";
-		String reverted = "dcba";
-		assertEquals(reverted, StringReverser.reverse(s));
+
+	private static final Object[] getInputValues() {
+		return $($("abcd", "dcba"), $("12345", "54321"));
 	}
-	
+
 	@Test
-	public void revertsStringOfNumbers() {
-		String s = "12345";
-		String reverted = "54321";
+	@Parameters(method = "getInputValues")
+	public void revertsCorrectly(String s, String reverted) {
 		assertEquals(reverted, StringReverser.reverse(s));
 	}
 
