8418306000a0548153915e3d2374d5f8542cdfbf
Diego Marcílio <dvmarcilio@gmail.com>
2014-03-10 21:28:03 -0300

    Refactoring tests.

--- src/test/java/study/chap3/HashMapTest.java
+++ src/test/java/study/chap3/HashMapTest.java
@@ -9,30 +9,44 @@ import java.util.Map;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(JUnitParamsRunner.class)
 public class HashMapTest {
 
+	private Map<Integer, String> map;
+
+	private static final String FIRST_VALUE = "FIRST";
+	private static final String SECOND_VALUE = "SECOND";
+	private static final String THIRD_VALUE = "THIRD";
+	private static final String DIFFERENT_VALUE = "DIFFERENT";
+
+	@Before
+	public void setUp() {
+		map = new HashMap<Integer, String>();
+	}
+
+	static final Object[] getInputs() {
+		return $($(1, FIRST_VALUE), $(2, SECOND_VALUE), $(3, THIRD_VALUE));
+	}
+
 	@Test
-	public void objectPutCanBeRetrievedUsingGet() throws Exception {
-		Map<Integer, String> map = new HashMap<Integer, String>();
-		Integer key = 1;
-		String value = "first";
+	@Parameters(method = "getInputs")
+	public void objectPutCanBeRetrievedUsingGet(Integer key, String value)
+			throws Exception {
 		map.put(key, value);
 		assertEquals(value, map.get(key));
 	}
 
 	@Test
-	public void addObjectWithExistentKeyUpdatesValue() throws Exception {
-		Map<Integer, String> map = new HashMap<Integer, String>();
-		Integer key = 1;
-		String value = "first";
+	@Parameters(method = "getInputs")
+	public void addObjectWithExistentKeyUpdatesValue(Integer key, String value)
+			throws Exception {
 		map.put(key, value);
-		String differentValue = "different";
-		map.put(key, differentValue);
-		assertEquals(differentValue, map.get(key));
+		map.put(key, DIFFERENT_VALUE);
+		assertEquals(DIFFERENT_VALUE, map.get(key));
 	}
 
 }
