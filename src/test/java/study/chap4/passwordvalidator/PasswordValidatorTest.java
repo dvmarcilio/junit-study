0c985703db5e2ddd049252961b38e1f2f212f4cb
Diego Marcílio <dvmarcilio@gmail.com>
2014-03-27 17:07:52 -0300

    Testing that password must have at least one digit

--- src/test/java/study/chap4/passwordvalidator/PasswordValidatorTest.java
+++ src/test/java/study/chap4/passwordvalidator/PasswordValidatorTest.java
@@ -12,24 +12,46 @@ import org.junit.runner.RunWith;
 
 @RunWith(JUnitParamsRunner.class)
 public class PasswordValidatorTest {
-	
+
 	@Rule
-	public ExpectedException illegalArgExc = ExpectedException.none();
+	public ExpectedException expectedException = ExpectedException.none();
+
+	@Test
+	@Parameters(method = "shortPasswords")
+	public void passwordShouldHaveAtLeastSixCharacters(String shortPassword) {
+		expectIllegalArgExcWithMessage(PasswordValidator.MUST_BE_LONGER_MESSAGE);
+		PasswordValidator.validate(shortPassword);
+	}
 
 	static final Object[] shortPasswords() {
 		return $($("12345"), $("1234"), $("123"), $("12"), $("1"), $(""));
 	}
 
+	private void expectIllegalArgExcWithMessage(String message) {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage(message);
+	}
+
 	@Test
-	@Parameters(method = "shortPasswords")
-	public void passwordWithLessThan6CharsShouldThrowIllegalArgExc(String password) {
-		expectIllegalArgExcWithMessage(PasswordValidator.SHORT_MESSAGE);
-		PasswordValidator.validate(password);
+	@Parameters(method = "validPasswords")
+	public void validatesValidPassword(String validPassword) throws Exception {
+		assertTrue(PasswordValidator.validate(validPassword));
+	}
+
+	static final Object[] validPasswords() {
+		return $($("a23456"), $("1bcdef"), $("abcde12345"));
+	}
+
+	@Test
+	@Parameters(method = "noDigitPasswords")
+	public void passwordShouldHaveAtLeastOneDigit(String passwordWithNoDigit)
+			throws Exception {
+		expectIllegalArgExcWithMessage(PasswordValidator.MUST_HAVE_DIGIT_MESSAGE);
+		PasswordValidator.validate(passwordWithNoDigit);
 	}
 	
-	private void expectIllegalArgExcWithMessage(String message) {
-		illegalArgExc.expect(IllegalArgumentException.class);
-		illegalArgExc.expectMessage(message);
+	static final Object[] noDigitPasswords() {
+		return $($("abcdef"), $("ABCDEF"), $("qwerty"));
 	}
 
 }
