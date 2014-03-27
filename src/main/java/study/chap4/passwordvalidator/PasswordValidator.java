d274847832de35282c24541eb08584e628ef9b8d
Diego Marcílio <dvmarcilio@gmail.com>
2014-03-27 17:09:30 -0300

    Silly impl: pwd must have >= 1 digit

--- src/main/java/study/chap4/passwordvalidator/PasswordValidator.java
+++ src/main/java/study/chap4/passwordvalidator/PasswordValidator.java
@@ -3,12 +3,14 @@ package study.chap4.passwordvalidator;
 public class PasswordValidator {
 
 	private static String password;
-	
-	public static final String SHORT_MESSAGE = "Password must have at least 6 characters";
+
+	public static final String MUST_BE_LONGER_MESSAGE = "Password must have at least 6 characters";
+	public static final String MUST_HAVE_DIGIT_MESSAGE = "Password must have at least one digit";
 
 	public static boolean validate(String password) {
 		setPassword(password);
 		validateLength();
+		validateDigit();
 		return true;
 	}
 
@@ -17,8 +19,20 @@ public class PasswordValidator {
 	}
 
 	private static void validateLength() {
-		if(password.length() < 6)
-			throw new IllegalArgumentException(SHORT_MESSAGE);
+		if (password.length() < 6)
+			throw new IllegalArgumentException(MUST_BE_LONGER_MESSAGE);
 	}
 
+	private static void validateDigit() {
+		if (!passwordHasDigit())
+			throw new IllegalArgumentException(MUST_HAVE_DIGIT_MESSAGE);
+	}
+
+	private static boolean passwordHasDigit() {
+		for (int i = 0; i < password.length(); i++) {
+			if (Character.isDigit(password.charAt(i)))
+				return true;
+		}
+		return false;
+	}
 }
